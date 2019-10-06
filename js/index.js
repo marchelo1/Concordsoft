f07b4947f049383d2926f3687b5bf707eb50dff7
marchelo1 <marchelo1_@msn.com>
2019-10-06 11:17:00 +0200

    update

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// const ss = document.querySelectorAll('.scrollspy');
+// M.ScrollSpy.init(ss, {});
+
+
+// var img = document.getElementById("myImg");
+// img.onclick = function () {
+//     modal.style.display = "block";
+// }
+
+// var span = document.getElementsByClassName("close")[0];
+
+// span.onclick = function () {
+//     modal.style.display = "none";
+
+// }
